d42571e3b16949406f5e72d179d5ad545364221d
Jean Thierry-Mieg <mieg@ncbi.nlm.nih.gov>
2026-01-09 13:00:30 -0500

    2026_01_09

--- wsa/run.sh
+++
@@ -1,4 +0,0 @@
-nvcc -x c++ -g -O4  -DACEDB4 \
- -I/usr/include/c++/8 -I/usr/include/c++/8/x86_64-redhat-linux \
- -I. -I.. -I../wh -I../wsa -I/netopt/sge/include \
- -DAC_TEST -DOPTERON -D_FILE_OFFSET_BITS=64 -c -Wno-deprecated-gpu-targets  -o sa.main.o sa.main.c
